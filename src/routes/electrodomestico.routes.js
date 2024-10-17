bbe96e2d9dee54fbcf2e8f5d73096a2d0c9f4f5c
IsidroTrevino <A00837104@tec.mx>
2024-10-17 08:10:50 -0600

    agregar mas endpoints a electrodomesticos

--- src/routes/electrodomestico.routes.js
+++ src/routes/electrodomestico.routes.js
@@ -53,4 +53,69 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    const {usuarioId, nombre, tipo, consumoWatts, descripcion, urlimagen} = req.body;
+
+    try {
+        const electrodomestico = await prisma.electrodomestico.create({
+            data: {
+                usuarioId,
+                nombre,
+                tipo,
+                consumoWatts,
+                descripcion,
+                urlimagen
+            }
+        });
+
+        res.json(electrodomestico);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, tipo, consumoWatts, descripcion, urlimagen } = req.body;
+
+    try {
+        const electrodomestico = await prisma.electrodomestico.update({
+            where: {
+                electrodomesticoId: parseInt(id)
+            },
+            data: {
+                nombre,
+                tipo,
+                consumoWatts,
+                descripcion,
+                urlimagen
+            }
+        });
+
+        res.json(electrodomestico);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await prisma.electrodomestico.delete({
+            where: {
+                electrodomesticoId: parseInt(id)
+            }
+        });
+
+        res.json({ message: 'Electrodomestico deleted' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 export default router;

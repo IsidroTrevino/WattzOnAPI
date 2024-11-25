f63e8f4ec16715258f8bcd017ebab45fbb938909
debbelzz <A00835550@tec.mx>
2024-11-24 22:09:08 -0600

    changed migration

---
+++ prisma/migrations/20241125040823_final/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `idConcepto` on the `Recibo` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Recibo" DROP COLUMN "idConcepto";
